c468d90920f0298fc9ccc7d1819467a701e248e9
suleiman-ade <okunoyesouleiman@gmail.com>
2026-03-19 11:35:34 +0100

    fixed sitemap issue

--- api/tools/pension-calculator.ts
+++
@@ -1,3 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import handler from './_handlers';
-export default (req: VercelRequest, res: VercelResponse) => handler('pension-calculator', req, res);
